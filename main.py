087a9113ad936ede4605a1c1bbee1c7c7d21fd57
bdr1945 <142620983+bdr1945@users.noreply.github.com>
2023-09-06 18:28:06 +0300

    dd

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # write your code here
+person = {
+    "name":"bader",
+    "age": 14,
+    'hobbies':['programming','video games','hunting']
+
+}
+print(person['name'])
+print(person['age'])
+
+person['age']= 19
+person["country"]= "kuwait"
+print(person)
+print(len(person))
+
+person['hobbies'].append('movies')
+def check_hobbies(user):
+    if len(user['hobbies']) > 3:
+        print('wow you are amazing')
+
+check_hobbies(person)
